43da515df29d2348ecf56f150ef78abb680e2ae1
AlexKostin <sebastian.hunter@mail.ru>
2026-04-12 21:22:25 +0500

    1_1_1: добавлен экспорт по умолчанию для компонента App

--- src/1/1_1_1/App.tsx
+++ src/1/1_1_1/App.tsx
@@ -1,11 +1,9 @@
-// Исправьте ошибку с импортом компонентов
-function App() {
+// Экспортируем компонент App по умолчанию, чтобы его можно было импортировать в других файлах (например, в main.tsx)
+export default function App() {
     return (
         <img
             src="AkliluLemma.jpg"
             alt="Aklilu Lemma"
         />
     );
-  }
-  
-  
+}
