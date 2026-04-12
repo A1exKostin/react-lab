af282ab632bb1cdc3b54de9a843961623740589c
AlexKostin <sebastian.hunter@mail.ru>
2026-04-12 22:15:53 +0500

    1_2_1: компонент Profile вынесен в отдельный файл Profile.tsx

---
+++ src/1/1_2_1/Profile.tsx
@@ -0,0 +1,9 @@
+// Компонент Profile вынесен в отдельный файл для лучшей читаемости и переиспользования.
+export default function Profile() {
+  return (
+    <img
+      src="AlanHart.jpg"
+      alt="Alan L. Hart"
+    />
+  );
+}
