676a7d34837b7df1ceff0d3fd2d84fd0f7c69949
AlexKostin <sebastian.hunter@mail.ru>
2026-04-12 22:29:32 +0500

    1_3_1: конвертирован HTML в JSX с исправлением синтаксических ошибок

--- src/1/1_3_1/App.tsx
+++ src/1/1_3_1/App.tsx
@@ -1,17 +1,16 @@
-// 1_3_1 Convert some HTML to JSX
-/* 
-  Этот HTML был вставлен в компонент, но это не корректный JSX. Исправьте его.
-*/
-
+// HTML конвертирован в JSX: добавлен пустой фрагмент для обертки возвращаемых элементов,
+// атрибуты class заменены на className, теги br сделаны самозакрывающимися (<br />), а также исправлен порядок закрытия тегов (сначала </i>, потом </b>).
 export default function Bio() {
-    return (
-      <div class="intro">
+  return (
+    <>
+      <div className="intro">
         <h1>Welcome to my website!</h1>
       </div>
-      <p class="summary">
+      <p className="summary">
         You can find my thoughts here.
-        <br><br>
-        <b>And <i>pictures</b></i> of scientists!
+        <br /><br />
+        <b>And <i>pictures</i></b> of scientists!
       </p>
-    );
-  }
+    </>
+  );
+}
