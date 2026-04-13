d41275ae1ce12c6b44257c90d1109931f474f016
AlexKostin <sebastian.hunter@mail.ru>
2026-04-13 12:24:29 +0500

    2_5_2: реализована функция getFinalState для имитации очереди обновлений состояния в React

--- src/2/2_5_2/processQueue.ts
+++ src/2/2_5_2/processQueue.ts
@@ -3,7 +3,15 @@ import { Que } from "./App";
 export function getFinalState(baseState: number, queue: Que[]) {
     let finalState = baseState;
 
-    // TODO: do something with the queue...
+    for (let update of queue) {
+        if (typeof update === 'function') {
+            // Если передан обновитель (функция), применяем его к текущему состоянию
+            finalState = update(finalState);
+        } else {
+            // Если передано конкретное значение, оно просто заменяет состояние
+            finalState = update;
+        }
+    }
 
     return finalState;
 }
