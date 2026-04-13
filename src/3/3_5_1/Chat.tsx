45feefae0a16f37603110d4eb40a7096b5b1ff3b
AlexKostin <sebastian.hunter@mail.ru>
2026-04-13 22:31:29 +0500

    3_5_1: реализована диспетчеризация экшенов в ContactList и Chat для работы Messenger через useReducer

--- src/3/3_5_1/Chat.tsx
+++ src/3/3_5_1/Chat.tsx
@@ -1,15 +1,14 @@
-import { useState } from 'react';
 import { Action } from './messengerReducer';
 import { Contact } from './App';
 
 export default function Chat(
+  {
+    contact, message, dispatch
+  }:
     {
-        contact, message, dispatch
-    }:
-    {
-        contact: Contact,
-        message: string,
-        dispatch: (action: Action) => void
+      contact: Contact,
+      message: string,
+      dispatch: (action: Action) => void
     }
 ) {
   return (
@@ -18,8 +17,10 @@ export default function Chat(
         value={message}
         placeholder={'Chat to ' + contact.name}
         onChange={(e) => {
-          // TODO: dispatch edited_message
-          // (Read the input value from e.target.value)
+          dispatch({
+            type: 'edited_message',
+            message: e.target.value
+          });
         }}
       />
       <br />
