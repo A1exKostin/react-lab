0ae5d4107b7adb1724a279440b72e3afcafe586e
AlexKostin <sebastian.hunter@mail.ru>
2026-04-13 10:56:02 +0500

    2_2_2: замена обычных переменных на useState для корректного обновления полей формы

--- src/2/2_2_2/App.tsx
+++ src/2/2_2_2/App.tsx
@@ -1,41 +1,40 @@
-// 2_2_2 Fix stuck form inputs
-/*
-  Когда вы вводите текст в поля ввода, ничего не появляется. Как будто входные значения "застряли" с пустыми строками. "Значение" первого <input> установлено так, чтобы всегда соответствовать переменной firstName, а "значение" второго <input> установлено так, чтобы всегда соответствовать переменной lastName. Это правильно. Оба входа имеют обработчики событий onChange, которые пытаются обновить переменные на основе последнего введенного пользователем значения (e.target.value). Однако переменные, похоже, не "запоминают" свои значения между повторными рендерингами. Исправьте это, используя вместо них переменные состояния.
-*/
+import { useState } from 'react';
 
+// Использование обычных переменных (let firstName) не заставляло React 
+// перерисовывать компонент после их изменения. Для сохранения введенных значений и рендера нового состояния, они заменены на хуки useState.
 export default function Form() {
-  let firstName = '';
-  let lastName = '';
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
 
-  function handleFirstNameChange(e: any) {
-      firstName = e.target.value;
-  }
+    function handleFirstNameChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setFirstName(e.target.value);
+    }
 
-  function handleLastNameChange(e: any) {
-      lastName = e.target.value;
-  }
+    function handleLastNameChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setLastName(e.target.value);
+    }
 
-  function handleReset() {
-      firstName = '';
-      lastName = '';
-  }
+    function handleReset() {
+        setFirstName('');
+        setLastName('');
+    }
 
-  return (
-      <form onSubmit={(e) => e.preventDefault()}>
-          <input
-              placeholder="First name"
-              value={firstName}
-              onChange={handleFirstNameChange}
-          />
-          <input
-              placeholder="Last name"
-              value={lastName}
-              onChange={handleLastNameChange}
-          />
-          <h1>
-              Hi, {firstName} {lastName}
-          </h1>
-          <button onClick={handleReset}>Reset</button>
-      </form>
-  );
+    return (
+        <form onSubmit={(e) => e.preventDefault()}>
+            <input
+                placeholder="First name"
+                value={firstName}
+                onChange={handleFirstNameChange}
+            />
+            <input
+                placeholder="Last name"
+                value={lastName}
+                onChange={handleLastNameChange}
+            />
+            <h1>
+                Hi, {firstName} {lastName}
+            </h1>
+            <button onClick={handleReset}>Reset</button>
+        </form>
+    );
 }
