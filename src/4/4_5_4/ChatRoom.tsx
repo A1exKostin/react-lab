ed545dd266ce0feeb400f1469eca9c6769e50ba0
AlexKostin <sebastian.hunter@mail.ru>
2026-04-14 15:37:49 +0500

    4_5_4: исправление переключения типа соединения путем добавления функции в зависимости useEffect

--- src/4/4_5_4/ChatRoom.tsx
+++ src/4/4_5_4/ChatRoom.tsx
@@ -1,15 +1,16 @@
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { Connection } from './chat';
 
 export default function ChatRoom({ roomId, createConnection }: {
     roomId: string; createConnection: (roomId: string) => Connection;
 }) {
     useEffect(() => {
+        // Создаем подключение, используя переданную функцию (которая может меняться)
         const connection = createConnection(roomId);
         connection.connect();
+        // Очищаем подключение при смене параметров или размонтировании
         return () => connection.disconnect();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [roomId]);
+    }, [roomId, createConnection]); // Добавляем createConnection в зависимости
 
     return <h1>Welcome to the {roomId} room!</h1>;
 }
