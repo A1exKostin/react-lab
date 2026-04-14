e8cb33d338ee7c6532088e89430a63a76646b5dd
AlexKostin <sebastian.hunter@mail.ru>
2026-04-14 13:08:55 +0500

    4_3_3: очистка интервала в useEffect через clearInterval

--- src/4/4_3_3/App.tsx
+++ src/4/4_3_3/App.tsx
@@ -1,13 +1,8 @@
-// 4_3_3 Fix an interval that fires twice
-/*
-  Этот компонент Counter отображает счетчик, который должен увеличиваться каждую секунду. При монтировании он вызывает setInterval. Это заставляет функцию onTick выполняться каждую секунду. Функция onTick увеличивает счетчик.
-
-  Однако вместо того, чтобы увеличиваться один раз в секунду, она увеличивается дважды. Почему так происходит? Найдите причину ошибки и исправьте ее.
-*/
 import { useState } from 'react';
 import Counter from './Counter.tsx';
 
 export default function Form() {
+  // Управление видимостью счетчика
   const [show, setShow] = useState(false);
   return (
     <>
--- src/4/4_3_3/Counter.tsx
+++ src/4/4_3_3/Counter.tsx
@@ -8,8 +8,12 @@ export default function Counter() {
       setCount(c => c + 1);
     }
 
-    setInterval(onTick, 1000);
-  }, []);
+    // Сохраняем ID интервала
+    const intervalId = setInterval(onTick, 1000);
+
+    // Возвращаем функцию очистки, которая сработает при размонтировании компонента
+    return () => clearInterval(intervalId);
+  }, []); // Пустой массив, интервал создается только один раз при монтировании
 
   return <h1>{count}</h1>;
 }
