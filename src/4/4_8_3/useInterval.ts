1712104944f498c8d169da565b453ef827374537
AlexKostin <sebastian.hunter@mail.ru>
2026-04-14 20:11:42 +0500

    4_8_3: извлечение логики интервала в отдельный хук useInterval

--- src/4/4_8_3/useInterval.ts
+++ src/4/4_8_3/useInterval.ts
@@ -1,5 +1,9 @@
-import { useEffect } from "react";
+import { useEffect } from 'react';
 
-export function useInterval(callback: () => void, delay: number) {
-    // Тут нужно реализовать хук
+// Общий хук для работы с интервалами
+export function useInterval(onTick: () => void, delay: number) {
+  useEffect(() => {
+    const id = setInterval(onTick, delay);
+    return () => clearInterval(id);
+  }, [onTick, delay]);
 }
